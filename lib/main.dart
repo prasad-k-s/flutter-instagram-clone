2e29f61b6b3222101b6765e50a1c0046b9d4e504
Prasad K S <prasad07062001@gmail.com>
2023-11-08 12:22:27 +0530

    add firebase config files

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/firebase_options.dart';
 import 'package:flutter_instagram_clone/responsive_layout/mobile_screen_layout.dart';
 import 'package:flutter_instagram_clone/responsive_layout/responsive.dart';
 import 'package:flutter_instagram_clone/responsive_layout/web_screen_layout.dart';
 import 'package:flutter_instagram_clone/utlis/colors.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const MyApp(),
   );
